e07cde101eb8afd358da46214cb18e8c06847ded
karulis <karulis@>
2014-11-15 22:31:30 +0100

    Test implementation for lescan method pt.2

--- bluez/ble.c
+++ bluez/ble.c
@@ -6,6 +6,209 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
+PyObject *ble_error;
+
+static volatile int signal_received = 0;
+
+static void sigint_handler(int sig)
+{
+    //TODO handle signal the python way;)
+    //signal_received = sig;
+}
+
+#define EIR_NAME_SHORT              0x08  /* shortened local name */
+#define EIR_NAME_COMPLETE           0x09  /* complete local name */
+
+static void eir_parse_name(uint8_t *eir, size_t eir_len,
+                        char *buf, size_t buf_len)
+{
+    size_t offset;
+
+    offset = 0;
+    while (offset < eir_len) {
+        uint8_t field_len = eir[0];
+        size_t name_len;
+
+        /* Check for the end of EIR */
+        if (field_len == 0)
+            break;
+
+        if (offset + field_len > eir_len)
+            goto failed;
+
+        switch (eir[1]) {
+        case EIR_NAME_SHORT:
+        case EIR_NAME_COMPLETE:
+            name_len = field_len - 1;
+            if (name_len > buf_len)
+                goto failed;
+
+            memcpy(buf, &eir[2], name_len);
+            return;
+        }
+
+        offset += field_len + 1;
+        eir += field_len + 1;
+    }
+
+failed:
+    snprintf(buf, buf_len, "(unknown)");
+}
+
+
+static int print_advertising_devices(int dd, uint8_t filter_type)
+{
+    unsigned char buf[HCI_MAX_EVENT_SIZE], *ptr;
+    struct hci_filter nf, of;
+    struct sigaction sa;
+    socklen_t olen;
+    int len;
+
+    olen = sizeof(of);
+    if (getsockopt(dd, SOL_HCI, HCI_FILTER, &of, &olen) < 0) {
+        printf("Could not get socket options\n");
+        return -1;
+    }
+
+    hci_filter_clear(&nf);
+    hci_filter_set_ptype(HCI_EVENT_PKT, &nf);
+    hci_filter_set_event(EVT_LE_META_EVENT, &nf);
+
+    if (setsockopt(dd, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0) {
+        printf("Could not set socket options\n");
+        return -1;
+    }
+
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_flags = SA_NOCLDSTOP;
+    sa.sa_handler = sigint_handler;
+    sigaction(SIGINT, &sa, NULL);
+
+    while (1) {
+        evt_le_meta_event *meta;
+        le_advertising_info *info;
+        char addr[18];
+
+        while ((len = read(dd, buf, sizeof(buf))) < 0) {
+            if (errno == EINTR && signal_received == SIGINT) {
+                len = 0;
+                goto done;
+            }
+
+            if (errno == EAGAIN || errno == EINTR)
+                continue;
+            goto done;
+        }
+
+        ptr = buf + (1 + HCI_EVENT_HDR_SIZE);
+        len -= (1 + HCI_EVENT_HDR_SIZE);
+
+        meta = (void *) ptr;
+
+        if (meta->subevent != 0x02)
+            goto done;
+
+        /* Ignoring multiple reports */
+        info = (le_advertising_info *) (meta->data + 1);
+//        if (check_report_filter(filter_type, info)) {
+            char name[30];
+
+            memset(name, 0, sizeof(name));
+
+            ba2str(&info->bdaddr, addr);
+            eir_parse_name(info->data, info->length,
+                            name, sizeof(name) - 1);
+
+            printf("%s %s\n", addr, name);
+//        }
+    }
+
+done:
+    setsockopt(dd, SOL_HCI, HCI_FILTER, &of, sizeof(of));
+
+    if (len < 0)
+        return -1;
+
+    return 0;
+}
+
+static void cmd_lescan(int dev_id, int opt)
+{
+    int err, dd;
+    uint8_t own_type = 0x00;
+    uint8_t scan_type = 0x01;
+    uint8_t filter_type = 0;
+    uint8_t filter_policy = 0x00;
+    uint16_t interval = htobs(0x0010);
+    uint16_t window = htobs(0x0010);
+    uint8_t filter_dup = 1;
+
+    switch (opt) {
+    case 'p':
+        own_type = 0x01; /* Random */
+        break;
+    case 'P':
+        scan_type = 0x00; /* Passive */
+        break;
+    case 'w':
+        filter_policy = 0x01; /* Whitelist */
+        break;
+    case 'd':
+        filter_type = optarg[0];
+        if (filter_type != 'g' && filter_type != 'l') {
+            fprintf(stderr, "Unknown discovery procedure\n");
+            exit(1);
+        }
+
+        interval = htobs(0x0012);
+        window = htobs(0x0012);
+        break;
+    case 'D':
+        filter_dup = 0x00;
+        break;
+    default:
+        printf("error");
+        return;
+    }
+
+    if (dev_id < 0)
+        dev_id = hci_get_route(NULL);
+
+    dd = hci_open_dev(dev_id);
+    if (dd < 0) {
+        perror("Could not open device");
+        exit(1);
+    }
+
+    err = hci_le_set_scan_parameters(dd, scan_type, interval, window,
+                        own_type, filter_policy, 10000);
+    if (err < 0) {
+        perror("Set scan parameters failed");
+        exit(1);
+    }
+
+    err = hci_le_set_scan_enable(dd, 0x01, filter_dup, 10000);
+    if (err < 0) {
+        perror("Enable scan failed");
+        exit(1);
+    }
+
+    printf("LE Scan ...\n");
+
+    err = print_advertising_devices(dd, filter_type);
+    if (err < 0) {
+        perror("Could not receive advertising events");
+        exit(1);
+    }
+
+    err = hci_le_set_scan_enable(dd, 0x00, filter_dup, 10000);
+    if (err < 0) {
+        perror("Disable scan failed");
+        exit(1);
+    }
+
+    hci_close_dev(dd);
+}
 
 PyObject *
 bt_lescan(PyObject *self, PyObject *args) {
@@ -25,31 +228,8 @@ bt_lescan(PyObject *self, PyObject *args) {
     err = PyList_Append( rtn_list, item_tuple );
     Py_DECREF( item_tuple );
 
+    //cmd_lescan(-1, 'd');
 
     return rtn_list;
 }
 
-/*
-static PyMethodDef ble_methods[] = {
-    {"lescan", lescan, METH_VARARGS, "lescan() doc string"},
-    {NULL, NULL}
-};
-
-static struct PyModuleDef blemodule = {
-    PyModuleDef_HEAD_INIT,
-    "ble",
-    "bluetooth low energy module",
-    -1,
-    ble_methods,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-};
-
-PyMODINIT_FUNC
-PyInit_ble(void)
-{
-    return PyModule_Create(&blemodule);
-}
-*/
